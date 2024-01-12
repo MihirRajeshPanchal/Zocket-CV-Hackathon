2d1380ebb942607c1a1534704c6d61555a279484
MihirRajeshPanchal <mihirpanchal5400@gmail.com>
2024-01-13 00:05:49 +0530

    deploy issue solved

--- src/main.tsx
+++ src/main.tsx
@@ -3,23 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { ChakraProvider, extendTheme, ColorModeScript } from '@chakra-ui/react';
 import Homepage from './components/SideBar.tsx';
 
-const theme = extendTheme({
-  config: {
-    initialColorMode: "dark",
-    useSystemColorMode: false, 
-  },
-});
-
-ColorModeScript.initialColorMode = { initialColorMode: 'dark' };
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-    {/* <Navbar/>
-    <App/> */}
-    <Homepage />
+    <ChakraProvider>
+      <Homepage />
     </ChakraProvider>
-
   </React.StrictMode>,
 )
